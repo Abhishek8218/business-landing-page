19073fded63c76afc633e17a2cf083bc6923ec65
Abhishek Kumar <ak3037308@gmail.com>
2024-02-13 16:33:16 +0530

    dummy text updated

--- src/About.jsx
+++ src/About.jsx
@@ -1,41 +1,48 @@
-
-
 const About = () => {
-  return (
-    <section className="pt-10 overflow-hidden bg-gray-200 md:pt-0 sm:pt-16 2xl:pt-16">
-    <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="grid items-center grid-cols-1 md:grid-cols-2">
-
+    return (
+      <section className="pt-10 overflow-hidden bg-gray-200 md:pt-0 sm:pt-16 2xl:pt-16">
+        <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+          <div className="grid items-center grid-cols-1 md:grid-cols-2">
             <div>
-                <h2 className="text-3xl font-sans font-bold leading-tight text-[#f8cf1c] sm:text-4xl lg:text-5xl">Hey 👋 I am
-                    <br className="block sm:hidden" /> Jenny Carter
-                </h2>
-                <p className="max-w-lg mt-3 text-xl leading-relaxed text-gray-500 md:mt-8">
-                    Amet minim mollit non deserunt
-                    ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
-                    Exercitation veniam consequat sunt nostrud amet.
-                </p>
-
-                <p className="mt-4 text-xl text-gray-600 md:mt-8">
-                    <span className="relative inline-block">
-                        <span className="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300"></span>
-                    <span className="relative"> Have a question? </span>
-                    </span>
-                    <br className="block sm:hidden" /> Ask me on <a href="#" title=""
-                        className="transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline">Twitter</a>
-                </p>
+              <h2 className="text-3xl font-sans font-bold leading-tight text-[#f8cf1c] sm:text-4xl lg:text-5xl">
+                Meet Darsheel Savla
+              </h2>
+              <p className="max-w-lg mt-3 text-xl leading-relaxed text-gray-500 md:mt-8">
+               
+Darsheel Savla, co-founder of PatilKaki, is a dynamic culinary leader launching a consultancy program for D2C entrepreneurs. Join him to supercharge your growth! 🚀📈
+              </p>
+              <p className="mt-4 text-xl text-gray-600 md:mt-8">
+                <span className="relative inline-block">
+                  <span className="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300"></span>
+                  <span className="relative">Have a question?</span>
+                </span>
+                <br className="block sm:hidden" /> Ask him on{' '}
+                <a
+                  href="#"
+                  title=""
+                  className="transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline"
+                >
+                  Twitter
+                </a>
+              </p>
             </div>
-
             <div className="relative">
-                <img className="absolute inset-x-0 bottom-0 -mb-48 -translate-x-1/2 left-1/2" src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/blob-shape.svg" alt="" />
-
-                <img className="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110" src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/business-woman.png" alt="" />
+              <img
+                className="absolute inset-x-0 bottom-0 -mb-48 -translate-x-1/2 left-1/2"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/blob-shape.svg"
+                alt=""
+              />
+              <img
+                className="relative w-full xl:max-w-lg xl:mx-auto 2xl:origin-bottom 2xl:scale-110"
+                src="https://cdn.rareblocks.xyz/collection/celebration/images/team/1/business-woman.png"
+                alt=""
+              />
             </div>
-
+          </div>
         </div>
-    </div>
-</section>
-  )
-}
-
-export default About
+      </section>
+    );
+  };
+  
+  export default About;
+  
